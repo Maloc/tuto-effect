e5d5917050712be1278c23013ef1922a2422d15a
Guillaume Hoarau <guillaume@merca.team>
2024-11-13 23:39:48 +0100

    feat: catchTags

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
-import { Console, Effect } from "effect";
+import { Effect } from 'effect';
 
-const main = Console.log("Hello world");
+interface FetchError {
+  readonly _tag: "FetchError";
+}
 
-Effect.runSync(main);
+interface JsonError {
+  readonly _tag: "JsonError";
+}
+
+const fetchRequest = Effect.tryPromise({
+  try: () => fetch("https://pokeapi.co/api/v2/psadokemon/garchomp/"),
+  catch: (): FetchError => ({ _tag: "FetchError" }),
+});
+
+const jsonResponse = (response: Response) =>
+  Effect.tryPromise({
+    try: () => response.json(),
+    catch: (): JsonError => ({ _tag: "JsonError" }),
+  });
+
+const main = fetchRequest.pipe(
+  Effect.flatMap(jsonResponse),
+  Effect.catchTags({
+    FetchError: () => Effect.succeed("Fetch error"),
+    JsonError: () => Effect.succeed("Json error"),
+  })
+);
+
+Effect.runPromise(main).then(console.log);
