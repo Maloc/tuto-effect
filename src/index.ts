636bc83607b34ca5a5e9b329784e046e20025cb3
Guillaume Hoarau <guillaume@merca.team>
2024-11-19 22:49:25 +0100

    refactor: generator

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,39 @@
-import { Effect } from 'effect';
+import { Data, Effect } from 'effect';
 
-interface FetchError {
-  readonly _tag: "FetchError";
-}
+/** Errors **/
+class FetchError extends Data.TaggedError("FetchError")<{}> { }
+class JsonError extends Data.TaggedError("JsonError")<{}> { }
 
-interface JsonError {
-  readonly _tag: "JsonError";
-}
 
+/** Implementation **/
 const fetchRequest = Effect.tryPromise({
-  try: () => fetch("https://pokeapi.co/api/v2/psadokemon/garchomp/"),
-  catch: (): FetchError => ({ _tag: "FetchError" }),
+  try: () => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"),
+  catch: () => new FetchError(),
 });
 
 const jsonResponse = (response: Response) =>
   Effect.tryPromise({
     try: () => response.json(),
-    catch: (): JsonError => ({ _tag: "JsonError" }),
+    catch: () => new JsonError(),
   });
 
-const main = fetchRequest.pipe(
-  Effect.flatMap(jsonResponse),
+const program = Effect.gen(function* () {
+  const response = yield* fetchRequest;
+  if (!response.ok) {
+    return yield* new FetchError();
+  }
+
+  return yield* jsonResponse(response);
+});
+
+/** Error handling **/
+const main = program.pipe(
   Effect.catchTags({
     FetchError: () => Effect.succeed("Fetch error"),
     JsonError: () => Effect.succeed("Json error"),
   })
 );
 
+
+/** Running effect **/
 Effect.runPromise(main).then(console.log);
